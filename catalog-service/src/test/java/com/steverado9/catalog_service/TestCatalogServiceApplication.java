f218b405702c80683c38c2561e935277142dbc4e
stephen <isaacsteverado9@gmail.com>
2026-07-13 23:21:41 +0100

    created the catalog-service

---
+++ catalog-service/src/test/java/com/steverado9/catalog_service/TestCatalogServiceApplication.java
@@ -0,0 +1,11 @@
+package com.steverado9.catalog_service;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(CatalogServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
