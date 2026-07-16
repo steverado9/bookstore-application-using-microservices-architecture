1e8c0b78c5b6b17cb76293bbcb4b7bf7b9e858e8
stephen <isaacsteverado9@gmail.com>
2026-07-16 01:58:35 +0100

    setup taskfile and created flyway migration scripts

---
+++ catalog-service/src/main/java/com/steverado9/catalog_service/web/controllers/ProductController.java
@@ -0,0 +1,13 @@
+package com.steverado9.catalog_service.web.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+//    List<Products> getProducts() {
+//        return productService.getProucts();
+//    }
+}
